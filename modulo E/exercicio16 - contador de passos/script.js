e72adf424499da4ca0e291eaa28d5cdc3b6b98f4
Emy-Liberato <emilly.lliberato@gmail.com>
2025-05-02 21:09:23 -0300

    corrigindo exercicios modulo E

--- modulo E/exercicio16 - contador de passos/script.js
+++ modulo E/exercicio16 - contador de passos/script.js
@@ -1,16 +1,40 @@
 function contar() {
-    let inicio = document.getElementById("inicio").value;
-    let fim = document.getElementById("fim").value;
-    let passo = document.getElementById("passo").value;
-    let resultado = document.getElementById("resultado");
+  let inicio = document.getElementById("inicio");
+  let fim = document.getElementById("fim");
+  let passo = document.getElementById("passo");
+  let res = document.getElementById("resultado");
 
-    inicio = Number(inicio);
-    fim = Number(fim);
-    passo = Number(passo);
+//  valida se a input está vazia 
+  if (
+    inicio.value.length == 0 ||
+    fim.value.length == 0 ||
+    passo.value.length == 0
+  ) {
+    res.innerHTML += "Impossivel contar!";
+  } else {
+    res.innerHTML = "Contando: </br>";
+    
+    // tranforma os dados em Number 
+    inicio = Number(inicio.value);
+    fim = Number(fim.value);
+    passo = Number(passo.value);
 
-    for (let i = inicio; i <= fim; i += passo) {
-        resultado.innerHTML += ` ${i} \u{1F449}`; 
+    if (passo <= 0) {
+        alert("Passo invalido! Consideramos passo = 1")
+        passo = 1;
     }
 
+    if (inicio < fim) {
+        // contagem crescente 
+      for (let i = inicio; i <= fim; i += passo) {
+        resultado.innerHTML += ` ${i} \u{1F449}`;
+      }
+    } else {
+        // contagem regressiva 
+        for (let i = inicio; i >= fim; i -= passo) {
+            resultado.innerHTML += ` ${i} \u{1F449}`;
+        }
+    }
     resultado.innerHTML += ` \u{1F3C1}`;
+  }
 }
--- modulo E/exercicio17 - tabuada/script.js
+++ modulo E/exercicio17 - tabuada/script.js
@@ -1,9 +1,22 @@
 function tabuada() {
-  var numero = Number(document.getElementById("numero").value);
-  var resultado = document.getElementById("resultado");
+  var numero = document.getElementById("numero");
+  var tab = document.getElementById("seltab");
   contador = 0;
 
-  for (var contador = 1; contador <= 10; contador++) {
-    resultado.innerHTML += `${contador} * ${numero} = ${contador * numero} <br>`;
+  if (numero.value.length == 0) {
+    alert("Por favor digite um número");
+  } else {
+    let n = Number(numero.value);
+
+    // limpa as options antes de fazer um novo calculo 
+    tab.innerHTML = ""
+
+    // faz o calculo e adiciona as options 
+    for (var contador = 1; contador <= 10; contador++) {
+      let item = document.createElement("option") //cria option
+      item.text =  `${n} * ${contador} = ${contador * n}` //coloca o texto na option
+      item.value = `tab${c}` // adiciona um valor para a identifiaçâo
+      tab.appendChild(item) // adiciona no select
+    }
   }
 }
